2f600ffc774cd8b19e5717975d669b814f748c4a
Chase Engel <chaselengel@gmail.com>
2024-03-09 10:34:51 -0800

    Configure more options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -27,3 +27,23 @@ vim.opt.cursorline = true
 
  -- otherwise it bounces in and out, not strictly needed though
 vim.opt.signcolumn = "yes"
+
+-- When substituting, opens a bottom split with matches that will be replaced
+vim.opt.inccommand = 'split'
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
