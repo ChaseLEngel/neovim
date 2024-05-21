40e5a34f17a5e5ba05bf7cbc7c6fc6f09a5b42ee
Chase Engel <chaselengel@gmail.com>
2024-05-21 12:54:57 -0700

    Change test maps

--- lua/user/test_runner.lua
+++ lua/user/test_runner.lua
@@ -13,5 +13,6 @@ local opts = { noremap = true, silent = false }
 local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<leader>tc", ":lua require('neotest').run.run()<CR>", opts)
+keymap("n", "<leader>tt", ":lua require('neotest').run.run()<CR>", opts)
 keymap("n", "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", opts)
 keymap("n", "<leader>to", ":lua require('neotest').output.open()<CR>", opts)
