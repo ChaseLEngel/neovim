8ca1f526d529d9918e53fcfbb2ecdf19dd10afa0
Chase Engel <chaselengel@gmail.com>
2025-04-22 07:58:29 -0700

    Configure other-nvim

--- lua/user/other.lua
+++ lua/user/other.lua
@@ -1,28 +1,33 @@
 require("other-nvim").setup({
-    mappings = {
-        "rails"
+  mappings = {
+    "rails",
+    {
+      pattern = "/app/models/(.*)%.rb",
+      target = "/app/controllers/%1s_controller.rb",
+      context = "controller"
     },
-    transformers = {
-        -- defining a custom transformer
-        lowercase = function (inputString)
-            return inputString:lower()
-        end
+    {
+      pattern = "/app/models/(.*)%.rb",
+      target = "/app/views/%1s/",
+      context = "view"
     },
-    style = {
-        -- How the plugin paints its window borders
-        -- Allowed values are none, single, double, rounded, solid and shadow
-        border = "solid",
+  },
+  showMissingFiles = false,
+  style = {
+    -- How the plugin paints its window borders
+    -- Allowed values are none, single, double, rounded, solid and shadow
+    border = "solid",
 
-        -- Column seperator for the window
-        seperator = "|",
+    -- Column seperator for the window
+    seperator = "|",
 
-	-- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
-	width = 0.7,
+    -- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
+    width = 0.7,
 
-	-- min height in rows.
-	-- when more columns are needed this value is extended automatically
-	minHeight = 2
-    },
+    -- min height in rows.
+    -- when more columns are needed this value is extended automatically
+    minHeight = 2
+  },
 })
 
 vim.api.nvim_set_keymap("n", "<leader>ol", "<cmd>:Other<CR>", { noremap = true, silent = true })
